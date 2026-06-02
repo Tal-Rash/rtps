ba61783757d7bdfd914b2f5f4d128a1947a255cd
Tal-Rash <shvarchkovsergey@gmail.com>
2026-06-03 00:13:10 +0300

    Do not launch Codex CLI from startup script

--- start-codex.bat
+++ start-codex.bat
@@ -3,4 +3,11 @@ setlocal
 cd /d "%~dp0" || exit /b 1
 git pull --ff-only
 if errorlevel 1 exit /b 1
-codex .
+echo.
+echo Latest changes are pulled.
+echo.
+echo Open Codex Desktop manually and select this folder:
+echo %CD%
+echo.
+start "" explorer.exe "%CD%"
+pause
