0bc7584a75811894af4916856e54e5a8c8ef9618
Tal-Rash <shvarchkovsergey@gmail.com>
2026-06-06 05:13:04 +0300

    Bump zamer KP version for manual loco entry

--- web_zamer_kp/app.py
+++ web_zamer_kp/app.py
@@ -25,7 +25,7 @@ DB_FILE = ROOT.parent / "base" / "common_database.db"
 SESSION_COOKIE = "grafik_ppr_session"
 SESSION_TTL_SECONDS = 7 * 24 * 60 * 60
 APP_PREFIX = "/zamer-kp"
-APP_VERSION = "web-zkp-1.17"
+APP_VERSION = "web-zkp-1.18"
 DB_LOCK = Lock()
 
 INPUT_ROWS = 12
