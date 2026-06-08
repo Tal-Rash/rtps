5fe0abcea1beeeece787952d3592d7b106a552b3
Tal-Rash <shvarchkovsergey@gmail.com>
2026-06-08 16:56:33 +0300

    Bump zamer version to 1.41

--- web_zamer_kp/app.py
+++ web_zamer_kp/app.py
@@ -27,7 +27,7 @@ DB_FILE = ROOT.parent / "base" / "common_database.db"
 SESSION_COOKIE = "grafik_ppr_session"
 SESSION_TTL_SECONDS = 7 * 24 * 60 * 60
 APP_PREFIX = "/zamer-kp"
-APP_VERSION = "web-zkp-1.40"
+APP_VERSION = "web-zkp-1.41"
 DB_LOCK = Lock()
 
 INPUT_ROWS = 12
