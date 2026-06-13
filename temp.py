34d8eacbdb3bcae5c1db482a4d4ee28a9068fb79
Tal-Rash <shvarchkovsergey@gmail.com>
2026-06-13 15:36:34 +0300

    Clean up temporary scripts

--- temp.py
+++
@@ -1,8 +0,0 @@
-import codecs
-
-with open(r'g:\Мой диск\Codex\Windows\src\Табель учета\Табель учета.py', 'rb') as f:
-    text = f.read().decode('utf-8', errors='ignore')
-
-for line in text.split('\n'):
-    if 'QColor' in line or 'txt in' in line or 'txt ==' in line:
-        print(line.strip())
